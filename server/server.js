975f626c8561511eb710727eb74da5edcbd3487d
Pascal Loose <pascal.loose@me.com>
2017-01-19 15:44:02 +0000

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
+const {ObjectID} = require('mongodb')
+
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
@@ -27,6 +29,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e)
   })
+})
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id
+
+  if(!ObjectID.isValid(id)) {
+    res.status(404).send('Sent ID not valid')
+  } else {
+    Todo.findById(id).then((todo) => {
+      if(!todo) {
+        res.status(404).send('Sent ID not found')
+      }
+      res.send({todo})
+    }).catch((e) => {
+      res.status(400).send()
+    })
+  }
 })
 
 app.listen(3000, () => {
