bcc3de8f1f3ed7d5e150cc3b8e17da3d5aae448a
Pascal Loose <pascal.loose@me.com>
2017-01-19 16:26:25 +0000

    Added test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,55 +1,55 @@
-var express = require('express')
-var bodyParser = require('body-parser')
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-const {ObjectID} = require('mongodb')
-
-var {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/todo')
-var {User} = require('./models/user')
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
 var app = express();
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
-  })
+  });
 
   todo.save().then((doc) => {
-    res.send(doc)
+    res.send(doc);
   }, (e) => {
-    res.status(400).send(e)
-  })
-})
+    res.status(400).send(e);
+  });
+});
 
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos})
+    res.send({todos});
   }, (e) => {
-    res.status(400).send(e)
-  })
-})
+    res.status(400).send(e);
+  });
+});
 
 app.get('/todos/:id', (req, res) => {
-  var id = req.params.id
+  var id = req.params.id;
 
-  if(!ObjectID.isValid(id)) {
-    res.status(404).send('Sent ID not valid')
-  } else {
-    Todo.findById(id).then((todo) => {
-      if(!todo) {
-        res.status(404).send('Sent ID not found')
-      }
-      res.send({todo})
-    }).catch((e) => {
-      res.status(400).send()
-    })
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
   }
-})
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 app.listen(3000, () => {
-  console.log('Started on port 3000')
-})
+  console.log('Started on port 3000');
+});
 
-module.exports = {app}
+module.exports = {app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,14 @@ const request = require('supertest')
 
 const {app} = require('./../server')
 const {Todo} = require('./../models/todo')
+const {ObjectID} = require('mongodb')
+
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }]
 
@@ -66,6 +70,34 @@ describe('GET /todos', () => {
       .expect((res) => {
         expect(res.body.todos.length).toBe(2)
       })
+      .end(done)
+  })
+})
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .expect(200)
+      .end(done)
+  })
+
+  it('should return 404 if todo not found', (done) => {
+    var id = new ObjectID()
+
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done)
+  })
+
+  it('should return 404 if todo is invalid', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(404)
       .end(done)
   })
 })
